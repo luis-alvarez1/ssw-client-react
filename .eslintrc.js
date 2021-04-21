2c8293737c53aa38bf02fb0a5de71c75a9f84b1e
Luis Alvarez <lealvarezp@unac.edu.co>
2021-04-21 12:37:51 -0500

    esliint config and ignore

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'no-console': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'no-return-await': 'off',
+    'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
+    indent: 'warn',
+    'no-unused-vars': 'warn',
+    'linebreak-style': 'off',
+    'consistent-return': 'off',
+    'function-paren-newline': 'off',
+    camelcase: 'off',
+    'import/prefer-default-export': 'off',
+    'jsx-quotes': 'off',
+    'object-curly-newline': 'off',
+    'react/self-closing-comp': 'warn',
+  },
+};
